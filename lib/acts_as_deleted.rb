84f917d6e9faafae1f6db87c50e648c8bbfbca2d
Kivanio Barbosa <kivanio@gmail.com>
2008-08-18 11:18:23 -0400

    moving methods

--- lib/acts_as_deleted.rb
+++ lib/acts_as_deleted.rb
@@ -3,22 +3,24 @@ module Acts #:nodoc:
   module As
     module Deleted
       def self.included(base) # :nodoc:
-        # base.extend(ClassMethods)
-        
-        base.named_scope :with_deleted, :conditions => {:deleted => true}
-        base.named_scope :without_deleted, :conditions => {:deleted => false}
-        
-        base.before_validation_on_create do |model|
-          model.deleted = 0 if model.respond_to?(:deleted)
-        end
+        base.extend(ClassMethods)
       end
 
       module ClassMethods
-       
+
+        def acts_as_deleted
+          self.named_scope :with_deleted, :conditions => {:deleted => true}
+          self.named_scope :without_deleted, :conditions => {:deleted => false}
+
+          self.before_validation_on_create do |model|
+            model.deleted = 0 if model.respond_to?(:deleted)
+          end
+        end
+
       end
 
       module InstanceMethods
-        
+
       end
     end
   end
